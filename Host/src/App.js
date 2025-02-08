140e1206e629ab1b60413ffc8c49e346770cc854
Tarun <trn18r@gmail.com>
2025-02-08 15:33:45 +0530

    chore: docker setup

---
+++ Host/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>App runnig on local</div>
+  )
+}
+
+export default App
--- Host/webpack.config.js
+++ Host/webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const path = require("path");
 
 module.exports = {
     mode:"development",
@@ -8,6 +9,38 @@ module.exports = {
         port:3000
     },
     output:{
-        
+       path: path.resolve(__dirname, "dist"),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"]
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html"
+        }),
+        // new ModuleFederationPlugin({
+        //     name: "host",
+        //     remotes: {
+        //         remote: "remote@http://localhost:3001/remoteEntry.js"
+        //     }
+        // })
+    ],
+    resolve: {
+        extensions: [".js", ".jsx"]
     }
 }
